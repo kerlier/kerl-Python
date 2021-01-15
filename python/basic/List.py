14278205f37b8895a15c2175b7c161e4d648d7d1
yangyuguang <yangyg@xinfud.com>
2021-01-15 09:26:02 +0800

    [ADD] list中的append以及extend的区别：append将整个参数当作元素。extend将参数list的元素加到当前list

--- python/basic/List.py
+++ python/basic/List.py
@@ -14,16 +14,21 @@ print(list[:4])   # 输出的是 a, b, c, d
 print(list[3:])   # 输出的是 d, e
 print(list[:])    # 输出list的所有元素
 print(list * 2)   # 输出两遍 list
-print(list + secondList)  # 拼接两个list
+print(list + secondList)  # 拼接两个list中的所有元素
+# list.append(secondList)   #append是将参数当成list中的一个元素。不管这个参数类型是List，还是其他类型
+# list.append(1)
+list.extend(secondList)  #extend是将参数list的所有元素放到当前list中
+print('extend:', list)
+
 
 # list的元素是可变的
 list[0] = 9       # 将list中第一个元素设置为9
 print(list)
 list[2:3] = [10]  # 将 list中第二个元素到第三个元素设置为10
-print(list)
+print('[2:3]', list)
 
-list[2:5] = []    # 将list设置为空
-print(list)
+list[2:5] = []    # 删除2-5位之间的元素
+print('[2:5]', list)
 
 
 # 翻转list
