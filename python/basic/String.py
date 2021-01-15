aa19939297a8503b6024af0a6d1f8a426ff4af8c
yangyuguang <yangyg@xinfud.com>
2021-01-15 09:32:17 +0800

    [ADD] str[1:1]是一个空白字符

--- python/basic/String.py
+++ python/basic/String.py
@@ -4,7 +4,7 @@
 str = "run python"
 
 print(str)     # 输出字符串
-print(str[0:0])  # 是一个空白字符串
+print(str[1:1])  # 是一个空白字符串
 print(str[0:-1])  # 输出第一个到倒数第二个的所有字符
 print(str[1])  # 输出index的字符
 print(str[2:3])  # 输出第二个字符到第三个字符的所有字符
