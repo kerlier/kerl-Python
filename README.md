c92d58aa8b011f29146d749646ca0ff8eb746ccf
yangyuguang <yangyg@xinfud.com>
2021-01-15 09:40:21 +0800

    [ADD]open文件时newLine的作用

--- README.md
+++ README.md
@@ -1,1 +1,5 @@
 # Python
+
+#### newLine的作用
+写文件时，会自动在行之间加入一个空白行，newLine是将这个空白行去掉
+![newLine作用](./python/img/new_line的作用.png)
--- python/csv_learn/2csv_reader_parsing_and_write.py
+++ python/csv_learn/2csv_reader_parsing_and_write.py
@@ -7,7 +7,7 @@ input_file = '../csv_data/input.csv'
 
 output_file = '../csv_data/output_with_csv.csv'
 
-with open(input_file, 'r', encoding='utf8', newline= '') as csv_in_file:
+with open(input_file, 'r', encoding='utf8', newline='') as csv_in_file:
     with open(output_file, 'w', encoding='utf8', newline='') as csv_out_file:
         # csv.reader 读取一个文件基本的fileReader，需要传一个delimiter
         filereader = csv.reader(csv_in_file, delimiter=',')
